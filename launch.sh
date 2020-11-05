f37b25d2b0a37daa2c1877d89752739c6e8974cd
Andrea F. Daniele <afdaniele@ttic.edu>
2020-11-05 17:05:14 -0600

    moved to template v2

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# YOUR CODE BELOW THIS LINE
-# ----------------------------------------------------------------------------
-roslaunch --wait drone_interface all.launch veh:=$VEHICLE_NAME \
-                              robot_type:=$ROBOT_TYPE
-
