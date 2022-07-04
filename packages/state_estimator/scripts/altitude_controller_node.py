a7f190c483b4a9fabd72555f8f33835b966e2ead
Vincenzo Polizzi <polivicio@gmail.com>
2022-07-04 21:09:51 +0200

    tmp push for altitude node controller

--- packages/state_estimator/scripts/altitude_controller_node.py
+++ packages/state_estimator/scripts/altitude_controller_node.py
@@ -3,13 +3,15 @@ import rospy
 from sensor_msgs.msg import  Range
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import DroneControl as RC
+from geometry_msgs.msg import Vector3
+from std_msgs.msg import Float32
 
 from simple_pid import PID
 
 
 class AltitudeControllerNode(DTROS):
     
-    ALTITUDE_SETPOINT = 0.2
+    ALTITUDE_SETPOINT = 0.3
 
     def __init__(self, node_name):
         # Initialize the DTROS parent class
@@ -22,11 +24,13 @@ class AltitudeControllerNode(DTROS):
         veh_name = rospy.get_namespace().rstrip('/')
 
         self.pid = PID(160, 110, 30, setpoint=self.ALTITUDE_SETPOINT)
-        self.pid.output_limits = (-200, 300)
+        self.pid.output_limits = (-100, 400)
 
         self._altitude_sub = rospy.Subscriber(
-            "altitude_node", Range, self.altitude_data_callback, queue_size=1)
-        self.cmd_publisher = rospy.Publisher("flight_controller_node/commands", RC, queue_size=1)
+            "altitude_node/altitude", Range, self.altitude_data_callback, queue_size=1)
+        self.cmd_publisher = rospy.Publisher("fly_commands_mux_node/autonomous", RC, queue_size=1)
+        self.pid_publisher = rospy.Publisher("alt_controller_node/pid", Vector3, queue_size=1)
+        self.err_publisher = rospy.Publisher("alt_controller_node/err", Float32, queue_size=1)
 
     def altitude_data_callback(self, msg):
         control = self.pid(msg.range)
@@ -35,9 +39,20 @@ class AltitudeControllerNode(DTROS):
         cmd_msg.roll = 1500
         cmd_msg.pitch = 1500
         cmd_msg.yaw = 1500
-        cmd_msg.throttle = 1200+ control
+        cmd_msg.throttle = 1100+ control
         p,i,d=self.pid.components
+        err = self.ALTITUDE_SETPOINT - msg.range
         print(f"Error: {(self.ALTITUDE_SETPOINT-msg.range)}, P: {p}, I: {i}, D: {d}")
+
+        pid_msg = Vector3()
+        pid_msg.x = p
+        pid_msg.y = i
+        pid_msg.z = d
+        self.pid_publisher.publish(pid_msg)
+
+        err_msg = Float32()
+        err_msg.data = err
+        self.err_publisher.publish(err_msg)
 
         self.cmd_publisher.publish(cmd_msg)
 
