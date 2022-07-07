35f2b0786e77a7c148cb5a736b21e7bc1ee8a3b9
Vincenzo Polizzi <polivicio@gmail.com>
2022-07-07 17:48:53 +0200

    fix for vincenzo's tests

--- packages/state_estimator/scripts/altitude_controller_node.py
+++ packages/state_estimator/scripts/altitude_controller_node.py
@@ -9,7 +9,7 @@ from simple_pid import PID
 
 class AltitudeControllerNode(DTROS):
     
-    ALTITUDE_SETPOINT = 0.2
+    ALTITUDE_SETPOINT = 0.3
 
     def __init__(self, node_name):
         # Initialize the DTROS parent class
@@ -22,11 +22,11 @@ class AltitudeControllerNode(DTROS):
         veh_name = rospy.get_namespace().rstrip('/')
 
         self.pid = PID(160, 110, 30, setpoint=self.ALTITUDE_SETPOINT)
-        self.pid.output_limits = (-200, 300)
+        self.pid.output_limits = (0, 400)
 
         self._altitude_sub = rospy.Subscriber(
-            "altitude_node", Range, self.altitude_data_callback, queue_size=1)
-        self.cmd_publisher = rospy.Publisher("flight_controller_node/commands", RC, queue_size=1)
+            "altitude_node/altitude", Range, self.altitude_data_callback, queue_size=1)
+        self.cmd_publisher = rospy.Publisher("fly_commands_mux_node/autonomous", RC, queue_size=1)
 
     def altitude_data_callback(self, msg):
         control = self.pid(msg.range)
@@ -35,14 +35,11 @@ class AltitudeControllerNode(DTROS):
         cmd_msg.roll = 1500
         cmd_msg.pitch = 1500
         cmd_msg.yaw = 1500
-        cmd_msg.throttle = 1200+ control
+        cmd_msg.throttle = 1100 + control
         p,i,d=self.pid.components
         print(f"Error: {(self.ALTITUDE_SETPOINT-msg.range)}, P: {p}, I: {i}, D: {d}")
 
         self.cmd_publisher.publish(cmd_msg)
-
-    def on_shutdown(self):
-        self.stop()
 
 
 if __name__ == "__main__":
