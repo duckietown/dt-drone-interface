c460295350516469f45dc2b1a3c7eb10aff2a64b
Andrea F. Daniele <afdaniele@ttic.edu>
2022-07-09 17:25:31 -0500

    bug fix

--- packages/pid_controller/scripts/pid_controller_node.py
+++ packages/pid_controller/scripts/pid_controller_node.py
@@ -135,7 +135,7 @@ class PIDController(DTROS):
         )
 
         # subscribers
-        rospy.Subscriber("~mode", Mode, self.current_mode_callback, queue_size=1)
+        rospy.Subscriber("~mode", Mode, self.current_mode_callback, queue_size=10)
         rospy.Subscriber("~state", Odometry, self.current_state_callback, queue_size=1)
         # TODO: to be refactored
         rospy.Subscriber("desired/pose", Pose, self.desired_pose_callback, queue_size=1)
@@ -407,6 +407,11 @@ def main(controller_class):
                 pid.reset()
                 # ---
                 pid.loginfo("Detected state change: ARMED -> FLYING")
+                pid.previous_mode = pid.current_mode
+
+            elif pid.current_mode == 0:
+                # ---
+                pid.loginfo("Detected state change: ARMED -> DISARMED")
                 pid.previous_mode = pid.current_mode
 
         # if the drone is flying, send the fly_command
